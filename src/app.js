a416431aeb37b8b462296048db45610e2b8bcbff
vivekchaurasiya12 <132990592+vivekchaurasiya12@users.noreply.github.com>
2025-11-04 12:35:02 +0530

    crud api done

--- src/app.js
+++ src/app.js
@@ -1,24 +1,10 @@
 
 const express = require("express");
-const {authRequest,userAuth} = require("./middleware/Auth")
+require("./config/database")
+
 const app = express();
-
-app.use("/admin",authRequest)
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("all data sent");
-})
-app.get("/admin/deleteAdmin",(req,res)=>{
-    res.send("Admin Deleted");
-})
-
-app.get("/user/getAll",userAuth,(req,res)=>{
-    res.send("all user sent");
-})
-
-
 
 
 app.listen(3000, () => {
-  console.log("✅ Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
