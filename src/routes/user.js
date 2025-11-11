e12c424810566f653c0640b6eaeb65994048b9dd
vivekchaurasiya12 <132990592+vivekchaurasiya12@users.noreply.github.com>
2025-11-12 01:27:34 +0530

    feed and paginaton api

--- src/routes/user.js
+++ src/routes/user.js
@@ -116,6 +116,40 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser.id }, { toUserId: loggedInUser.id }],
+    }).select("fromUserId  toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser.id   } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 /*
   ============================================================
    📦 MODULE EXPORT
